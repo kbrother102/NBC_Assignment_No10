9bdccc9636d1e07547c14bab71c799d4e5922c57
kbrother102 <kbrother102@gmail.com>
2026-03-27 19:27:56 +0900

    Module생성

---
+++ Source/MySpartaLog/MySpartaLog.Build.cs
@@ -0,0 +1,26 @@
+﻿using UnrealBuildTool;
+
+public class MySpartaLog : ModuleRules
+{
+    public MySpartaLog(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
---
+++ Source/MySpartaLog/Private/MySpartaLog.cpp
@@ -0,0 +1,17 @@
+﻿#include "MySpartaLog.h"
+
+#define LOCTEXT_NAMESPACE "FMySpartaLogModule"
+
+void FMySpartaLogModule::StartupModule()
+{
+    
+}
+
+void FMySpartaLogModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FMySpartaLogModule, MySpartaLog)
